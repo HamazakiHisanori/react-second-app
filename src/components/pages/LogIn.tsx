be359550c2bed9520228438583525376f9d22907
HamazakiHisanori <hiii1023@icloud.com>
2021-09-28 04:57:39 +0900

    Login Page create

--- src/components/pages/LogIn.tsx
+++ src/components/pages/LogIn.tsx
@@ -1,5 +1,22 @@
 import { memo, VFC } from "react";
+import { Box, Divider, Flex, Heading, Stack } from "@chakra-ui/layout";
+import { Input } from "@chakra-ui/input";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const LogIn: VFC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} ox={10}>
+          <Input placeholder="ユーザーID" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
