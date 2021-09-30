1c85fd8d952a029ed882f05d55a0bdf1ddbd8d90
HamazakiHisanori <hiii1023@icloud.com>
2021-09-30 13:29:24 +0900

    Message hooks create

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,11 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
   const [loading, setLoading] = useState<boolean>(false);
 
   const login = useCallback(
@@ -16,19 +18,29 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({
+              title: "ログインしました",
+              status: "success",
+            });
             history.push("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({
+              title: "ユーザーが見つかりません",
+              status: "error",
+            });
           }
         })
         .catch(() => {
-          alert("ログインできません");
+          showMessage({
+            title: "ログインに失敗しました",
+            status: "error",
+          });
         })
         .finally(() => {
           setLoading(false);
         });
     },
-    [history]
+    [history, showMessage]
   );
 
   return { login, loading };
