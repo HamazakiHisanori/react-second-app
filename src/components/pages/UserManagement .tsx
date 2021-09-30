4fc02f1c5253bb67ce61e476b60db77631e4845b
HamazakiHisanori <hiii1023@icloud.com>
2021-09-30 14:22:32 +0900

    UserManagement Page create

--- src/components/pages/UserManagement .tsx
+++ src/components/pages/UserManagement .tsx
@@ -1,19 +1,34 @@
-import { memo, VFC } from "react";
+import { memo, useEffect, VFC } from "react";
 import { Wrap } from "@chakra-ui/react";
-import { WrapItem } from "@chakra-ui/layout";
+import { Center, WrapItem } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), [getUsers]);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          image="https://source.unsplash.com/random"
-          nickName="sample"
-          fullName="sample.sample"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center height="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                image="https://source.unsplash.com/random"
+                nickName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
